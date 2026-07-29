4980724ea82451d18321f39c733a47989bc4f07a
QQQEB159 <2724758997@qq.com>
2026-07-29 23:39:19 +0800

    fuckFuck

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -18,7 +18,7 @@ import backend.DebugDisplay;
 	public var screensaver:Bool = false;
 	public var wideScreen:Bool = false;
 	#if android
-	public var storageType:String = "EXTERNAL";
+	public var storageType:String = "EXTERNAL_DATA";
 	#end
 	public var hitboxType:String = "Gradient";
 	public var downScroll:Bool = false;
